25fa2a43858ba4702e4074f81bccbabe4fd12110
Agus Dwi Sasongko <agusdwisasongko1@gmail.com>
2021-06-27 16:07:02 +0700

    Routes controller

---
+++ api/controllers/login_controller.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/agusdwis/blog-app/api/auth"
+	"github.com/agusdwis/blog-app/api/responses"
+	"github.com/agusdwis/blog-app/api/utils/formaterror"
+	"github.com/agusdwis/blog-app/models"
+)
+
+func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user.Prepare()
+	err = user.Validate("login")
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	token, err := server.SignIn(user.Email, user.Password)
+	if err != nil {
+		formatedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, formatedError)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, token)
+}
+
+func (server *Server) SignIn(email, password string) (string, error) {
+	var err error
+
+	user := models.User{}
+
+	err = server.DB.Debug().Model(models.User{}).Where("email = ?", email).Take(&user).Error
+	if err != nil {
+		return "", err
+	}
+
+	err = models.ValidatePassword(user.Password, password)
+	if err != nil {
+		return "", err
+	}
+
+	return auth.CreateToken(user.ID)
+}
